5cb3722f98c6f65b1d88254e8cf7f3256a763c20
Anthony Guerrand <59973442+wiziki-75@users.noreply.github.com>
2024-04-08 20:49:09 +0200

    Add files via upload

--- gestionevenements.sql
+++ gestionevenements.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Mar 18, 2024 at 11:16 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
+-- Hôte : 127.0.0.1
+-- Généré le : lun. 08 avr. 2024 à 20:47
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `gestionevenements`
+-- Base de données : `gestionevenements`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `evenement`
+-- Structure de la table `evenement`
 --
 
 CREATE TABLE `evenement` (
@@ -39,18 +39,18 @@ CREATE TABLE `evenement` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `evenement`
+-- Déchargement des données de la table `evenement`
 --
 
 INSERT INTO `evenement` (`idEvenement`, `nom`, `description`, `date`, `type`, `statut`, `organisateurId`, `lieuId`) VALUES
 (10, 'Festival musique', 'test', '2024-03-02 10:00:00', 'Concert', 'annulé', 11, 5),
-(11, 'Meeting aérien', 'Pour illustrer la différence entre une procédure stockée et une fonction stockée, prenons un exemple simple basé sur une base de données fictive qui stocke des informations sur des employés. Nous allons créer une procédure stockée pour insérer un nouvel employé dans la base de données et une fonction stockée pour calculer l\'âge d\'un employé à partir de sa date de naissance.', '2024-03-15 13:00:00', 'Communautaire', 'confirmé', 28, 3),
 (18, 'La route de la soif', 'alcool', '2024-03-07 02:14:00', 'Communautaire', 'confirmé', 11, 6),
-(19, 'test', 'test', '2024-03-24 17:21:00', 'Concert', 'confirmé', 11, 4),
-(20, 'Combat doumbe', 'ufc', '2024-03-20 02:29:00', 'Educatif', 'en_attente', 11, 7);
+(21, 'test', 'test', '2024-03-02 10:00:00', 'Concert', 'en_attente', 11, 4),
+(22, 'test3', 'test3', '2023-03-03 00:00:00', 'Concert', 'en_attente', 11, 7),
+(23, 'test4', 'test4', '2023-03-03 00:00:00', 'Concert', 'en_attente', 49, 7);
 
 --
--- Triggers `evenement`
+-- Déclencheurs `evenement`
 --
 DELIMITER $$
 CREATE TRIGGER `delete_participation` BEFORE DELETE ON `evenement` FOR EACH ROW BEGIN
@@ -84,53 +84,58 @@ DELIMITER ;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `lieu`
+-- Structure de la table `lieu`
 --
 
 CREATE TABLE `lieu` (
   `idLieu` int(11) NOT NULL,
-  `nom` varchar(255) NOT NULL,
-  `adresse` varchar(255) NOT NULL,
-  `capacite` int(11) NOT NULL,
+  `nom` varchar(30) NOT NULL,
+  `adresse` varchar(30) NOT NULL,
+  `capacite` varchar(20) NOT NULL,
   `disponibilite` enum('disponible','réservé','indisponible') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `lieu`
+-- Déchargement des données de la table `lieu`
 --
 
 INSERT INTO `lieu` (`idLieu`, `nom`, `adresse`, `capacite`, `disponibilite`) VALUES
-(2, 'Lieu 88', '456 Avenue des Fleurs', 67, 'indisponible'),
-(3, 'Lieu 3', '789 Boulevard du Parc', 200, 'réservé'),
-(4, 'Lieu 4', '101 Rue de la Gare', 150, 'réservé'),
-(5, 'Lieu 5', '202 Avenue de la République', 400, 'réservé'),
-(6, 'Lieu 69', '69 rue', 40, 'réservé'),
-(7, 'Parc des grands', 'Boulevard 69', 300, 'réservé'),
-(10, 'Gymnase leo lagrange', 'avenue gambetta', 800, 'indisponible');
+(3, 'Lieu 3', '789 Boulevard du Parc', '200', 'disponible'),
+(4, 'Lieu 4', '101 Rue de la Gare', '150', 'réservé'),
+(5, 'Lieu 5', '202 Avenue de la République', '400', 'réservé'),
+(6, 'Lieu 69', '69 rue', '40', 'réservé'),
+(7, 'Parc des grands', 'Boulevard 69', '300', 'réservé'),
+(28, 'test2', 'test2', '50', 'disponible'),
+(29, 'test2', 'test2', '50', 'disponible'),
+(30, 'test3', 'test3', 'test3', 'indisponible'),
+(31, 'test3', 'test3', 'test3', 'indisponible');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `organisateur`
+-- Structure de la table `organisateur`
 --
 
 CREATE TABLE `organisateur` (
   `idOrganisateur` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
   `service` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `organisateur`
+-- Déchargement des données de la table `organisateur`
 --
 
-INSERT INTO `organisateur` (`idOrganisateur`, `service`) VALUES
-(11, NULL),
-(28, NULL);
+INSERT INTO `organisateur` (`idOrganisateur`, `nom`, `prenom`, `service`) VALUES
+(11, 'Guerrand', 'Anthony', NULL),
+(28, 'Yolo', 'Jean', NULL),
+(49, 'test9', 'test9', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `participant`
+-- Structure de la table `participant`
 --
 
 CREATE TABLE `participant` (
@@ -140,24 +145,23 @@ CREATE TABLE `participant` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `participant`
+-- Déchargement des données de la table `participant`
 --
 
 INSERT INTO `participant` (`idParticipant`, `dateinscription`, `nbEnfants`) VALUES
 (9, '2024-01-19 00:00:00', NULL),
 (11, '2024-01-22 00:00:00', NULL),
 (15, '2024-01-25 00:00:00', NULL),
-(17, '2024-01-25 00:00:00', NULL),
-(18, '2024-01-25 00:00:00', NULL),
 (29, '2024-03-01 00:00:00', NULL),
-(30, '2024-03-01 00:00:00', NULL),
 (31, '2024-03-14 00:00:00', NULL),
-(32, '2024-03-15 00:00:00', NULL);
+(47, '2024-04-08 00:00:00', NULL),
+(48, '2024-04-08 00:00:00', NULL),
+(50, '2024-04-08 00:00:00', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `participation`
+-- Structure de la table `participation`
 --
 
 CREATE TABLE `participation` (
@@ -169,18 +173,15 @@ CREATE TABLE `participation` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `participation`
+-- Déchargement des données de la table `participation`
 --
 
 INSERT INTO `participation` (`idParticipation`, `idParticipant`, `dateinscription`, `nbenfants`, `idEvenement`) VALUES
-(39, 28, '2024-03-01 09:40:47', 0, 11),
-(42, 30, '2024-03-01 09:47:57', 0, 11),
 (48, 11, '2024-03-06 23:28:49', 0, 18),
-(49, 11, '2024-03-07 13:11:09', 0, 19),
-(50, 31, '2024-03-14 08:53:50', 0, 11);
+(54, 33, '2024-04-04 08:22:07', 0, 18);
 
 --
--- Triggers `participation`
+-- Déclencheurs `participation`
 --
 DELIMITER $$
 CREATE TRIGGER `check_participation` BEFORE INSERT ON `participation` FOR EACH ROW BEGIN
@@ -202,7 +203,7 @@ DELIMITER ;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Structure de la table `user`
 --
 
 CREATE TABLE `user` (
@@ -216,23 +217,23 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `user`
+-- Déchargement des données de la table `user`
 --
 
 INSERT INTO `user` (`idUtilisateur`, `nom`, `prenom`, `courriel`, `motdepasse`, `resetMDP`, `role`) VALUES
 (9, 'Lopez', 'Manon', 'manon.lopez@email.com', 'password106', 1, 'participant'),
 (11, 'Guerrand', 'Anthony', 'anthony.guerrand92@gmail.com', 'root', NULL, 'organisateur'),
 (15, 'lourd', 'zark', 'z@gmail.com', '$2y$10$OzY4JlFJen1G0rNZOLvRgu0KCxzJ4aZRcZYdAd4.g1dLwlfjKFCrm', NULL, 'participant'),
-(17, 'Guerrand', 'Dole', 'dole.guerrand92@gmail.com', '$2y$10$luig2wTqW38UP59aTzL7WuV.S0Kt9BhX96Fg5SoC54N7VbbCO0KdO', NULL, 'participant'),
-(18, 'Guerrand', 'Anthony', 'anthony.guerrand2@gmail.com', '$2y$10$TAzI77e21LPAbIxthUICSOD5mdQR0ofsnu7FcZt0VNj/YlGH3gbVW', NULL, 'participant'),
 (28, 'Yolo', 'Jean', 'jy@gmail.com', '$2y$10$mDPJWkc2EIbgTs1zoehAl.qmXr5C8kUWsxakal.ex/F5ZiFukBG2i', NULL, 'organisateur'),
 (29, 'MICHEL', 'LOUIS', 'lm@gmail.com', '$2y$10$vxSdTvbFWgPxvC8T2A80RuNrMuZWIRqhI8fRMSekDbS2/jq82HTby', NULL, 'participant'),
-(30, 'Ulm', 'Jack', 'ju@gmail.com', '$2y$10$Je/Z9uYAl0xra06gULuWq.7.j2RHHKH/.14CmpnsUcTTOwvE8BvAK', 0, 'participant'),
 (31, 'Migro', 'Timoté', 'tm@gmail.com', 'root', 0, 'participant'),
-(32, 'lala', 'lolo', 'lala@gmail.com', 'lala', NULL, 'participant');
+(47, 'test7', 'test7', 'test7', 'test7', 0, 'participant'),
+(48, 'test8', 'test8', 'test8', 'test8', 0, 'participant'),
+(49, 'test9', 'test9', 'test9', 'test9', 0, 'organisateur'),
+(50, 'test', 'test', 'test', 'test', 0, 'participant');
 
 --
--- Triggers `user`
+-- Déclencheurs `user`
 --
 DELIMITER $$
 CREATE TRIGGER `check_email` BEFORE INSERT ON `user` FOR EACH ROW BEGIN
@@ -277,11 +278,13 @@ END
 $$
 DELIMITER ;
 DELIMITER $$
-CREATE TRIGGER `insert_user` AFTER INSERT ON `user` FOR EACH ROW BEGIN 
+CREATE TRIGGER `insert_user` AFTER INSERT ON `user` FOR EACH ROW BEGIN
     IF NEW.role = 'organisateur' THEN
-        INSERT INTO organisateur (idOrganisateur, service) VALUES (NEW.idUtilisateur, NULL);
+        INSERT IGNORE INTO organisateur (idOrganisateur, nom, prenom, service) 
+        VALUES (NEW.idUtilisateur, NEW.nom, NEW.prenom, NULL);
     ELSEIF NEW.role = 'participant' THEN
-        INSERT INTO participant (idParticipant, dateinscription, nbEnfants) VALUES (NEW.idUtilisateur, CURDATE(), NULL);
+        INSERT IGNORE INTO participant (idParticipant, dateinscription, nbEnfants) 
+        VALUES (NEW.idUtilisateur, CURDATE(), NULL);
     END IF;
 END
 $$
@@ -289,24 +292,28 @@ DELIMITER ;
 DELIMITER $$
 CREATE TRIGGER `update_user` AFTER UPDATE ON `user` FOR EACH ROW BEGIN
     IF NEW.role != OLD.role THEN
-      IF NEW.role = 'organisateur' THEN
-          INSERT INTO organisateur (idOrganisateur, service) VALUES (NEW.idUtilisateur, NULL);
-          DELETE FROM participant WHERE idParticipant = NEW.idUtilisateur;
-      ELSEIF NEW.role = 'participant' THEN
-          INSERT INTO participant (idParticipant, dateinscription, nbEnfants) VALUES (NEW.idUtilisateur, CURDATE(), NULL);
-          DELETE FROM organisateur WHERE idOrganisateur = NEW.idUtilisateur;
-      END IF;
+        IF NEW.role = 'organisateur' THEN
+            INSERT INTO organisateur (idOrganisateur, nom, prenom, service) 
+            VALUES (NEW.idUtilisateur, NEW.nom, NEW.prenom, NULL)
+            ON DUPLICATE KEY UPDATE nom = VALUES(nom), prenom = VALUES(prenom), service = VALUES(service);
+            DELETE FROM participant WHERE idParticipant = NEW.idUtilisateur;
+        ELSEIF NEW.role = 'participant' THEN
+            INSERT INTO participant (idParticipant, dateinscription, nbEnfants) 
+            VALUES (NEW.idUtilisateur, CURDATE(), NULL)
+            ON DUPLICATE KEY UPDATE dateinscription = VALUES(dateinscription), nbEnfants = VALUES(nbEnfants);
+            DELETE FROM organisateur WHERE idOrganisateur = NEW.idUtilisateur;
+        END IF;
     END IF;
 END
 $$
 DELIMITER ;
 
 --
--- Indexes for dumped tables
+-- Index pour les tables déchargées
 --
 
 --
--- Indexes for table `evenement`
+-- Index pour la table `evenement`
 --
 ALTER TABLE `evenement`
   ADD PRIMARY KEY (`idEvenement`),
@@ -314,90 +321,90 @@ ALTER TABLE `evenement`
   ADD KEY `lieuId` (`lieuId`);
 
 --
--- Indexes for table `lieu`
+-- Index pour la table `lieu`
 --
 ALTER TABLE `lieu`
   ADD PRIMARY KEY (`idLieu`);
 
 --
--- Indexes for table `organisateur`
+-- Index pour la table `organisateur`
 --
 ALTER TABLE `organisateur`
   ADD PRIMARY KEY (`idOrganisateur`);
 
 --
--- Indexes for table `participant`
+-- Index pour la table `participant`
 --
 ALTER TABLE `participant`
   ADD PRIMARY KEY (`idParticipant`);
 
 --
--- Indexes for table `participation`
+-- Index pour la table `participation`
 --
 ALTER TABLE `participation`
   ADD PRIMARY KEY (`idParticipation`),
   ADD KEY `idEvenement` (`idEvenement`);
 
 --
--- Indexes for table `user`
+-- Index pour la table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`idUtilisateur`),
   ADD UNIQUE KEY `courriel` (`courriel`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
--- AUTO_INCREMENT for table `evenement`
+-- AUTO_INCREMENT pour la table `evenement`
 --
 ALTER TABLE `evenement`
-  MODIFY `idEvenement` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `idEvenement` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
--- AUTO_INCREMENT for table `lieu`
+-- AUTO_INCREMENT pour la table `lieu`
 --
 ALTER TABLE `lieu`
-  MODIFY `idLieu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `idLieu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
--- AUTO_INCREMENT for table `participation`
+-- AUTO_INCREMENT pour la table `participation`
 --
 ALTER TABLE `participation`
-  MODIFY `idParticipation` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+  MODIFY `idParticipation` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
 
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `idUtilisateur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `idUtilisateur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 
 --
--- Constraints for dumped tables
+-- Contraintes pour les tables déchargées
 --
 
 --
--- Constraints for table `evenement`
+-- Contraintes pour la table `evenement`
 --
 ALTER TABLE `evenement`
   ADD CONSTRAINT `evenement_ibfk_1` FOREIGN KEY (`organisateurId`) REFERENCES `organisateur` (`idOrganisateur`),
   ADD CONSTRAINT `evenement_ibfk_2` FOREIGN KEY (`lieuId`) REFERENCES `lieu` (`idLieu`);
 
 --
--- Constraints for table `organisateur`
+-- Contraintes pour la table `organisateur`
 --
 ALTER TABLE `organisateur`
   ADD CONSTRAINT `organisateur_ibfk_1` FOREIGN KEY (`idOrganisateur`) REFERENCES `user` (`idUtilisateur`);
 
 --
--- Constraints for table `participant`
+-- Contraintes pour la table `participant`
 --
 ALTER TABLE `participant`
   ADD CONSTRAINT `participant_ibfk_1` FOREIGN KEY (`idParticipant`) REFERENCES `user` (`idUtilisateur`);
 
 --
--- Constraints for table `participation`
+-- Contraintes pour la table `participation`
 --
 ALTER TABLE `participation`
   ADD CONSTRAINT `participation_ibfk_1` FOREIGN KEY (`idEvenement`) REFERENCES `evenement` (`idEvenement`);
